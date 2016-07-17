6a8d71258d90991b9e94bf54cf66ad11a49bf542
Cedric Song <tcsong@thoughtworks.com>
2016-07-17 14:18:12 +0800

    Problem 65 again: match zero

--- p65a/Solution.py
+++ p65a/Solution.py
@@ -1,8 +1,12 @@
+import re
+
 class Solution(object):
     def isNumber(self, s):
         """
         :type s: str
         :rtype: bool
         """
+        if s == "":
+            return False
 
-        return False
+        return re.match(r"0", s)
--- p65a/test_solution.py
+++ p65a/test_solution.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
+from p65a.Solution import Solution
+
 
 class TestSolution(TestCase):
   def test(self):
     self.assertTrue(True)
+
+  def test_integer(self):
+    solution = Solution()
+    self.assertEqual(solution.isNumber(""), False)
+
+    self.assertTrue(solution.isNumber("0"), True)
 
