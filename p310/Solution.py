8aebe8e3eb80a75fd6b6f783c6c573b9fa3be6bd
Cedric Song <tcsong@thoughtworks.com>
2017-09-11 21:35:47 +0800

    P310: construct tree

--- p310/Solution.py
+++ p310/Solution.py
@@ -5,3 +5,17 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+
+    def createTree(self, N, E):
+        tree = {}
+        for i in range(N):
+            tree[i+1] = { "edges": [], "conn": 0 }
+
+        for e in E:
+            tree[e[0]]["edges"].append(e[1])
+            tree[e[1]]["edges"].append(e[0])
+
+            tree[e[0]]["conn"] += 1
+            tree[e[1]]["conn"] += 1
+
+        return tree
--- p310/test_solution.py
+++ p310/test_solution.py
@@ -7,5 +7,14 @@ __author__ = 'tcsong'
 class TestSolution(TestCase):
   def setUp(self):
     self.sol = Solution()
-  def test_true(self):
-    self.assertEqual(1,1)
+
+  def test_construct_trees(self):
+    N = 3
+    E = [[1,2],[2,3]]
+    tree = self.sol.createTree(N, E)
+    self.assertEqual(len(tree[1]["edges"]), 1)
+    self.assertEqual(len(tree[2]["edges"]), 2)
+
+    self.assertEqual(tree[1]["conn"], 1)
+    self.assertEqual(tree[2]["conn"], 2)
+
