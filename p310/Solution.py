461359dffbc685178ebfaf835c271cbec060b705
Cedric Song <tcsong@thoughtworks.com>
2017-09-11 22:23:18 +0800

    P310: calc height

--- p310/Solution.py
+++ p310/Solution.py
@@ -1,3 +1,4 @@
+MAX_NUM = 10000000000
 class Solution(object):
     def findMinHeightTrees(self, n, edges):
         """
@@ -9,7 +10,10 @@ class Solution(object):
     def createTree(self, N, E):
         tree = {}
         for i in range(N):
-            tree[i+1] = { "edges": [], "conn": 0 }
+            tree[i] = { "edges": [],
+                          "conn": 0,
+                          "height": MAX_NUM,
+                          "visit": False}
 
         for e in E:
             tree[e[0]]["edges"].append(e[1])
@@ -25,5 +29,26 @@ class Solution(object):
         for k in tree:
             if tree[k]["conn"] == 1:
                 conn_nodes.append(k)
+                tree[k]["height"] = 1
+                tree[k]["visit"] = True
         return conn_nodes
 
+    def calc_minimum_height(self, tree):
+        inhand = self.get_conn_nodes(tree)
+        while len(inhand) > 0:
+            uu = inhand.pop(0)
+            u = tree[uu]
+            print (uu, u)
+
+            for vv in u["edges"]:
+                v = tree[vv]
+
+                if v["height"] > u["height"] + 1:
+                    v["height"] = u["height"] + 1
+
+                if v["conn"] > 0:
+                    v["conn"] -= 1
+
+                if v["visit"] == False:
+                    v["visit"] = True
+                    inhand.append(vv)
--- p310/test_solution.py
+++ p310/test_solution.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from p310.Solution import Solution
+MAX_NUM = 10000000000
 
 __author__ = 'tcsong'
 
@@ -10,20 +11,36 @@ class TestSolution(TestCase):
 
   def test_construct_trees(self):
     N = 3
-    E = [[1,2],[2,3]]
+    E = [[0,1],[1,2]]
     tree = self.sol.createTree(N, E)
-    self.assertEqual(len(tree[1]["edges"]), 1)
-    self.assertEqual(len(tree[2]["edges"]), 2)
+    self.assertEqual(len(tree[0]["edges"]), 1)
+    self.assertEqual(len(tree[1]["edges"]), 2)
 
-    self.assertEqual(tree[1]["conn"], 1)
-    self.assertEqual(tree[2]["conn"], 2)
+    self.assertEqual(tree[0]["conn"], 1)
+    self.assertEqual(tree[1]["conn"], 2)
+
+    self.assertEqual(tree[0]["height"], 1)
+    self.assertEqual(tree[1]["height"], MAX_NUM)
 
   def test_get_conn_1_nodes(self):
     N = 3
-    E = [[1,2],[2,3]]
+    E = [[0,1],[1,2]]
     tree = self.sol.createTree(N, E)
     inHandNodes = self.sol.get_conn_nodes(tree)
     self.assertEqual(len(inHandNodes), 2)
-    self.assertTrue(1 in [1,3])
-    self.assertTrue(3 in [1,3])
+    self.assertTrue(0 in [0,2])
+    self.assertTrue(2 in [0,2])
+
+    self.assertEqual(tree[0]["height"], 1)
+    self.assertEqual(tree[1]["height"], MAX_NUM)
+
+  def test_calc_minimum_height(self):
+    N = 6
+    E = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+    tree = self.sol.createTree(N, E)
+    self.sol.calc_minimum_height(tree)
+    self.assertEqual(tree[3]['height'],2)
+    self.assertEqual(tree[4]['height'],2)
+
+
 
