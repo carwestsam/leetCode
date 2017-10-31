0331e54da1ae3201f1375817ec04172e735eb84e
carwestsam <carwestsam@gmail.com>
2017-11-01 02:21:01 +0800

    finish p23

--- p23/Solution.py
+++ p23/Solution.py
@@ -6,9 +6,67 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def mergeKLists(self, lists):
+    def swap(self, lists, a, b):
+        t = lists[a]
+        lists[a] = lists[b]
+        lists[b] = t
+
+    def down(self, lists, idx):
+        size = len(lists)
+        l = (idx+1) * 2 - 1
+        r = l + 1
+        if l < size:
+            t = l
+            if r < size and lists[l].val > lists[r].val:
+                t = r
+
+            if lists[t].val < lists[idx].val:
+                self.swap(lists, t, idx)
+                self.down(lists, t)
+
+    def mergeKLists(self, olists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        return lists[0]
+
+        lists = []
+
+        for node in olists:
+            if node != None:
+                lists.append(node)
+
+        if len(lists) == 0:
+            return None
+
+        for x in range(len(lists)//2, -1, -1):
+            self.down(lists, x)
+
+        ans = []
+
+        while len(lists) > 0:
+            ans.append(lists[0])
+
+            if lists[0].next == None:
+                if len(lists) > 1:
+                    lists[0] = lists[len(lists) - 1]
+                    del lists[len(lists)-1]
+                    self.down(lists, 0)
+                else :
+                    del lists[0]
+
+            else :
+                lists[0] = lists[0].next
+                self.down(lists, 0)
+
+        for x in range(len(ans) - 1):
+            ans[x].next = ans[x+1]
+
+        ans[len(ans) -1].next = None
+
+        # p = ans[0]
+        # while p != None:
+        #     print(p.val)
+        #     p = p.next
+
+        return ans[0]
--- p23/test_solution.py
+++ p23/test_solution.py
@@ -13,3 +13,26 @@ class TestSolution(TestCase):
     ans = sol.mergeKLists([n1])
 
     self.assertEqual(ans, n1)
+
+  def test_should_return_merged_list(self):
+    sol = Solution()
+    n11 = ListNode(1)
+    n12 = ListNode(3)
+    n11.next = n12
+
+    n21 = ListNode(2)
+    n22 = ListNode(6)
+    n21.next = n22
+
+    n31 = ListNode(4)
+    n32 = ListNode(5)
+    n31.next = n32
+
+    ans = sol.mergeKLists([n21, n31, n11])
+
+    self.assertEqual(ans, n11)
+
+  def test_special(self):
+    sol = Solution()
+    ans = sol.mergeKLists([None])
+    self.assertEqual(ans, None)
