226f4dcf5da05e65162bde5b2b8f2f76816215b7
carwestsam <carwestsam@gmail.com>
2017-11-24 23:21:28 +0800

    p21: finish

--- p21/Solution.py
+++ p21/Solution.py
@@ -11,4 +11,32 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        return []
+        p1 = l1
+        p2 = l2
+
+        head = None
+        pt = None
+        next = None
+
+        while not ( (p1 == None ) and ( p2 == None)):
+            if p2 == None:
+                next = p1
+                p1 = p1.next
+            elif p1 == None:
+                next = p2
+                p2 = p2.next
+            else:
+                if p1.val <= p2.val:
+                    next = p1
+                    p1 = p1.next
+                else:
+                    next = p2
+                    p2 = p2.next
+
+            if head == None:
+                head = pt = next
+            else:
+                pt.next = next
+                pt = next
+
+        return head
--- p21/test_solution.py
+++ p21/test_solution.py
@@ -1,9 +1,45 @@
 from unittest import TestCase
 from p21.Solution import Solution
 
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+def listLike(node):
+  l = []
+  pt = node
+  while pt != None:
+    l.append(pt.val)
+    pt = pt.next
+  return l
+
+def nodeLike(l):
+  next = None
+  pt = None
+  for x in reversed(l):
+    pt = ListNode(x)
+    pt.next = next
+    next = pt
+  return pt
+
 class TestSolution(TestCase):
   def setUp(self):
     self.sol = Solution()
+
   def test_should_return_empty_list(self):
-    result = self.sol.mergeTwoLists([], [])
-    self.assertEqual(result, [])
+    result = self.sol.mergeTwoLists(None, None)
+    self.assertEqual(result, None)
+
+  def test_should_return_non_empty_list(self):
+    n2 = ListNode(2)
+    n1 = ListNode(1)
+    n1.next = n2
+
+    self.assertEqual(listLike(self.sol.mergeTwoLists(n1,None)), [1,2])
+
+  def test_should_return_merged_list(self):
+    l1 = nodeLike([1,3,5])
+    l2 = nodeLike([2,4,6])
+
+    self.assertEqual(listLike(self.sol.mergeTwoLists(l1, l2)), [1,2,3,4,5,6])
