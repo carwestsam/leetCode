961dfb38a5b80277550060a22607477a9b801554
Cedric Sung <carwestsam@gmail.com>
2018-11-12 16:56:58 +0800

    P1: part3, random order

--- p1/Solution.py
+++ p1/Solution.py
@@ -6,7 +6,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
+
         N = len(nums)
+
+        advanced_list = []
+
+        for i in range(N):
+            advanced_list.append([nums[i], i])
+
+        advanced_list.sort(key=lambda x: x[0])
+
 
         for i in range(N - 1):
             l = i+1
@@ -14,11 +23,13 @@ class Solution(object):
             m = (l + r)/2
 
             while l <= r:
-                if nums[i] + nums[m] == target:
-                    return [i, m]
-                if nums[i] + nums[m] > target:
+                if advanced_list[i][0] + advanced_list[m][0] == target:
+                    ans = [advanced_list[i][1], advanced_list[m][1]]
+                    ans.sort()
+                    return ans
+                if advanced_list[i][0] + advanced_list[m][0] > target:
                     r = m - 1
-                elif nums[i] + nums[m] < target:
+                elif advanced_list[i][0] + advanced_list[m][0] < target:
                     l = m + 1
                 m = (l+r)/2
 
--- p1/test_solution.py
+++ p1/test_solution.py
@@ -14,3 +14,8 @@ class TestSolution(TestCase):
         self.assertEqual(sol.twoSum([3, 23, 435, 1234], 1237), [0, 3])
         self.assertEqual(sol.twoSum([1, 3, 4, 5, 6, 7, 8, 9, 15, 33], 24), [7, 8])
         self.assertEqual(sol.twoSum([1, 3, 4, 5, 6, 7, 8, 9, 15, 33], 23), [6, 8])
+
+    def test_find_two_in_random_array(self):
+        sol = Solution()
+        self.assertEqual([0, 3], sol.twoSum([5, 3, 2, 1], 6))
+        self.assertEqual([1, 7], sol.twoSum([32, 23, 512, 522, 123, 412, 512, 64, 1, 67, 124], 87))
