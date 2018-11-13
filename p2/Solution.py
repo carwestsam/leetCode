8a6c9414f3f4a2c2138b6896bd31f666b702a448
Cedric Sung <carwestsam@gmail.com>
2018-11-13 22:18:33 +0800

    P2: without new number

--- p2/Solution.py
+++ p2/Solution.py
@@ -12,5 +12,30 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+        p1 = l1
+        p2 = l2
+        ans = None
+        last = None
 
-        return ListNode(l1.val + l2.val)
+        while p1 is not None and p2 is not None:
+
+            if p1 is not None:
+                v1 = p1.val
+                p1 = p1.next
+            else:
+                v1 = 0
+            if p2 is not None:
+                v2 = p2.val
+                p2 = p2.next
+            else:
+                v2 = 0
+
+            if ans == None:
+                ans = ListNode(v1 + v2)
+                last = ans
+            else:
+                node = ListNode(v1 + v2)
+                last.next = node
+                last = node
+
+        return ans
--- p2/test_solution.py
+++ p2/test_solution.py
@@ -15,3 +15,16 @@ class TestSolution(TestCase):
         l1 = ListNode(2)
         l2 = ListNode(4)
         self.assertEqual(6, sol.addTwoNumbers(l1, l2).val)
+
+    def test_add_without_push_forwad(self):
+        sol = Solution()
+        l11 = ListNode(2)
+        l12 = ListNode(3)
+        l11.next = l12
+
+        l21 = ListNode(3)
+        l22 = ListNode(4)
+        l21.next = l22
+
+        self.assertEqual(5, sol.addTwoNumbers(l11, l21).val)
+        self.assertEqual(7, sol.addTwoNumbers(l11, l21).next.val)
