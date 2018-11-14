9eb1866e00c9efe614605a23e9e200ad095374b5
Cedric Sung <carwestsam@gmail.com>
2018-11-14 10:22:32 +0800

    P3: longest unrepeat string by looping

--- p3/Solution.py
+++ p3/Solution.py
@@ -4,7 +4,14 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        pass
+        ans = 0
+
+        for i in range(len(s)):
+            result = self.lengthOfLongestSubstringIndex(s, i)
+            if result > ans:
+                ans = result
+
+        return ans
 
     def lengthOfLongestSubstringIndex(self, seq, idx):
         currentDict = {}
@@ -17,4 +24,3 @@ class Solution:
                 ans += 1
 
         return ans
-
