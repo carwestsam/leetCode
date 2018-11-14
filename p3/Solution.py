4db4178367e584e90111f63bc82dddb24a10064d
Cedric Sung <carwestsam@gmail.com>
2018-11-14 10:17:43 +0800

    P3: longest unrepeat string from specific position

--- p3/Solution.py
+++ p3/Solution.py
@@ -5,3 +5,16 @@ class Solution:
         :rtype: int
         """
         pass
+
+    def lengthOfLongestSubstringIndex(self, seq, idx):
+        currentDict = {}
+        ans = 0
+        for i in range(idx, len(seq)):
+            if seq[i] in currentDict:
+                break
+            else:
+                currentDict[seq[i]] = i
+                ans += 1
+
+        return ans
+
--- p3/test_solution.py
+++ p3/test_solution.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
+from p3.Solution import Solution
+
 
 class TestSolution(TestCase):
     def test_lengthOfLongestSubstring(self):
         self.assertTrue(True)
+
+    def test_should_get_longest_substring_from_specific_position(self):
+        sol = Solution()
+
+        self.assertEqual(3, sol.lengthOfLongestSubstringIndex("abcabcbb", 0))
+        self.assertEqual(1, sol.lengthOfLongestSubstringIndex("bbbbb", 2))
+        self.assertEqual(3, sol.lengthOfLongestSubstringIndex("pwwkew", 2))
