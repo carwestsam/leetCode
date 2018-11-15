eec791d0684453f03e86a8231fdac6757b6b97d0
Cedric Sung <carwestsam@gmail.com>
2018-11-15 17:47:57 +0800

    P4: get by index && no overlap

--- p4/Solution.py
+++ p4/Solution.py
@@ -5,4 +5,31 @@ class Solution:
         :type nums2: List[int]
         :rtype: float
         """
-        pass
+        l1 = len(nums1)
+        l2 = len(nums2)
+
+        idx1 = (l1 + l2 + 1) // 2
+        idx2 = (l1 + l2 + 2) // 2
+
+        if idx1 != idx2:
+            return (self.findByIndex(nums1, nums2, idx1) + self.findByIndex(nums1, nums2, idx2)) / 2
+        else :
+            return self.findByIndex(nums1, nums2, idx1)
+
+
+    def findByIndex(self, nums1, nums2, idx):
+        l1 = len(nums1)
+        l2 = len(nums2)
+
+        if nums1[0] > nums2[-1] or nums1[-1] < nums2[0]:
+            if nums1[0] > nums2[-1]:
+                if idx > l2:
+                    return nums1[idx - l2 - 1]
+                else:
+                    return nums2[idx - 1]
+
+            if nums2[0] > nums1[-1]:
+                if idx > l1:
+                    return nums2[idx - l1 - 1]
+                else:
+                    return nums1[idx - 1]
--- p4/test_solution.py
+++ p4/test_solution.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
+from p4.Solution import Solution
+
 
 class TestSolution(TestCase):
     def test_findMedianSortedArrays(self):
         self.assertTrue(True)
+
+    def test_find_median_of_no_overlap_arrays(self):
+        sol = Solution()
+        self.assertEqual(2, sol.findMedianSortedArrays([1], [2, 3]))
+        self.assertEqual(8, sol.findMedianSortedArrays([1, 2], [7, 8, 9, 10, 11]))
+        self.assertEqual(8, sol.findMedianSortedArrays([7, 8, 9, 10, 11], [1, 2]))
+        self.assertEqual(8.5, sol.findMedianSortedArrays([7, 8, 9, 10, 11, 12], [1, 2]))
