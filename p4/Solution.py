5fc2d41bd9b027d908026caa7737c227b87f0a81
Cedric Sung <carwestsam@gmail.com>
2018-11-16 12:58:27 +0800

    P4: n * log(n) solution

--- p4/Solution.py
+++ p4/Solution.py
@@ -13,13 +13,17 @@ class Solution:
 
         if idx1 != idx2:
             return (self.findByIndex(nums1, nums2, idx1) + self.findByIndex(nums1, nums2, idx2)) / 2
-        else :
+        else:
             return self.findByIndex(nums1, nums2, idx1)
-
 
     def findByIndex(self, nums1, nums2, idx):
         l1 = len(nums1)
         l2 = len(nums2)
+
+        if l1 == 0:
+            return nums2[idx - 1]
+        elif l2 == 0:
+            return nums1[idx - 1]
 
         if nums1[0] > nums2[-1] or nums1[-1] < nums2[0]:
             if nums1[0] > nums2[-1]:
@@ -33,3 +37,33 @@ class Solution:
                     return nums2[idx - l1 - 1]
                 else:
                     return nums1[idx - 1]
+
+        else:
+            result = self.findSpecificIndex(nums1, nums2, idx)
+            if result is None:
+                result = self.findSpecificIndex(nums2, nums1, idx)
+            return result
+
+    def findSpecificIndex(self, nums1, nums2, targetIdx):
+        for i in range(len(nums1)):
+            ii, left, right = self.findScope(nums2, nums1[i], 0, len(nums2))
+            # print(i, nums1[i], ii, left, right)
+            if left + i == targetIdx - 1:
+                return nums1[i]
+            if left + i > targetIdx - 1:
+                break
+
+    # return ( max index of less or equal (include), left part size, right part size )
+    def findScope(self, nums, target, left, right):
+        l = left
+        r = right
+        m = (l + r + 1) // 2
+        while l + 1 < r:
+            if nums[m] > target:
+                r = m
+            elif nums[m] <= target:
+                l = m
+
+            m = (l + r + 1) // 2
+
+        return (l, l - left + 1, right - l - 1)
--- p4/test_solution.py
+++ p4/test_solution.py
@@ -13,3 +13,25 @@ class TestSolution(TestCase):
         self.assertEqual(8, sol.findMedianSortedArrays([1, 2], [7, 8, 9, 10, 11]))
         self.assertEqual(8, sol.findMedianSortedArrays([7, 8, 9, 10, 11], [1, 2]))
         self.assertEqual(8.5, sol.findMedianSortedArrays([7, 8, 9, 10, 11, 12], [1, 2]))
+
+    def test_find_by_index_in_one_empty_list(self):
+        sol = Solution()
+
+        self.assertEqual(7, sol.findByIndex([], [1, 3, 5, 7], 4))
+        self.assertEqual(4, sol.findMedianSortedArrays([], [1, 3, 5, 7]))
+        self.assertEqual(7, sol.findByIndex([1, 3, 5, 7], [], 4))
+        self.assertEqual(4, sol.findMedianSortedArrays([1, 3, 5, 7], []))
+
+    def test_find_scope(self):
+        sol = Solution()
+
+        self.assertEqual((3, 4, 1), sol.findScope([1, 2, 3, 4, 5], 4, 0, 5))
+        self.assertEqual((3, 4, 1), sol.findScope([1, 2, 4, 4, 5], 4, 0, 5))
+        self.assertEqual((4, 5, 3), sol.findScope([1, 2, 4, 4, 5, 6, 7, 8], 5.5, 0, 8))
+        self.assertEqual((4, 2, 2), sol.findScope([1, 2, 4, 4, 5, 6, 7, 8], 5.5, 3, 7))
+
+    def test_find_by_index_in_two_array(self):
+        sol = Solution()
+
+        self.assertEqual(3, sol.findByIndex([1, 3, 5, 7], [2, 4, 6, 8], 3))
+        self.assertEqual(4, sol.findByIndex([1, 3, 5, 7], [2, 4, 6, 8], 4))
