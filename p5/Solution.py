84b2d6bafbc50a16dd694adc2239011ec0975ab2
Cedric Sung <carwestsam@gmail.com>
2018-11-16 22:46:36 +0800

    P5: O(n^3) Solution

--- p5/Solution.py
+++ p5/Solution.py
@@ -5,3 +5,23 @@ class Solution:
         :rtype: str
         """
         return s
+
+    def longestMatch(self, origin, pattern):
+        longest = 0
+        longestIdx = -1
+        lo = len(origin)
+        lp = len(pattern)
+        for j in range(lp):
+            for i in range(lo):
+                tmp = 0
+                for k in range(lp - j):
+                    if i+k < lo and pattern[j+k] == origin[i+k]:
+                        tmp += 1
+                    else:
+                        break
+                if tmp > longest:
+                    longest = tmp
+                    longestIdx = i
+
+        print(longestIdx, longest)
+        return origin[longestIdx:longestIdx+longest]
--- p5/test_solution.py
+++ p5/test_solution.py
@@ -7,3 +7,7 @@ class TestSolution(TestCase):
     def test_longestPalindrome(self):
         sol = Solution()
         sol.longestPalindrome("a")
+
+    def test_should_find_by_conbine_reverse_string(self):
+        sol = Solution()
+        self.assertEqual("aba", sol.longestMatch("abaa", "aaba"))
