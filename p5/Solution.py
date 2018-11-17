c554ab8779cf035bcd12cb9bcbb7cb59c15d7547
Cedric Sung <carwestsam@gmail.com>
2018-11-17 20:17:40 +0800

    P5: n^2 solution

--- p5/Solution.py
+++ p5/Solution.py
@@ -4,26 +4,41 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        return s
+        if len(s) <= 1:
+            return s
+        return self.longestMatch(s, s[::-1])
 
     def longestMatch(self, origin, pattern):
         longest = 0
         longestIdx = -1
         lo = len(origin)
         lp = len(pattern)
-        for j in range(lp):
-            for i in range(lo):
-                tmp = 0
-                for k in range(lp - j):
-                    if i + k < lo and pattern[j + k] == origin[i + k]:
-                        tmp += 1
-                    else:
-                        break
-                if tmp > longest:
-                    longest = tmp
-                    longestIdx = i
+        init = [-1] * (lp+1)
 
-        print(longestIdx, longest)
+        for j in range(lp - 1):
+            self.kmpInit(pattern, j-1, init)
+
+            k = j-1
+            i = 0
+
+            while i < lo:
+                while k != j-1 and k+1 < lp and origin[i] != pattern[k+1]:
+                    k = init[k]
+
+                if k+1 < lp and origin[i] == pattern[k + 1]:
+                    k += 1
+
+                if k - j + 1 > longest and j + i + 1 == lp:
+                    longest = k - j + 1
+
+                    longestIdx = i - longest + 1
+
+
+                if k+1 == lp:
+                    k = init[k]
+
+                i += 1
+
         return origin[longestIdx:longestIdx + longest]
 
     def kmpInit(self, pattern, left, init):
--- p5/test_solution.py
+++ p5/test_solution.py
@@ -11,6 +11,9 @@ class TestSolution(TestCase):
     def test_should_find_by_conbine_reverse_string(self):
         sol = Solution()
         self.assertEqual("aba", sol.longestMatch("abaa", "aaba"))
+        self.assertEqual("aba", sol.longestMatch("babad", "dabab"))
+        self.assertEqual("bb", sol.longestMatch("cbbd", "dbbc"))
+        self.assertEqual("bb", sol.longestMatch("bb", "bb"))
 
     def test_should_calc_string_kmp_init_value(self):
         sol = Solution()
@@ -18,3 +21,14 @@ class TestSolution(TestCase):
         self.assertEqual([-1, -1, 0, 1, 2, 3, -1], sol.kmpInit("abababc", -1, init)[0:7])
         self.assertEqual([-1, -1, 0, 1, 2, 3, -1, 0, 1, 2, 3], sol.kmpInit("abababcabab", -1, init)[0:11])
         self.assertEqual([9, 9, 9, 10, 11, 12, 9], sol.kmpInit("aaaaaaaaaaabcabcd", 9, init)[10:17])
+
+    def test_should_find_maxium_reverse_string(self):
+        sol = Solution()
+        self.assertEqual("aaaaa", sol.longestPalindrome("aaaaa"))
+        self.assertEqual("aabaa", sol.longestPalindrome("aabaa"))
+        self.assertEqual("aabaa", sol.longestPalindrome("aabaaxd"))
+        self.assertEqual("g", sol.longestPalindrome("abcdefg"))
+        self.assertEqual("a", sol.longestPalindrome("a"))
+        self.assertEqual("b", sol.longestPalindrome("ab"))
+        self.assertEqual("aa", sol.longestPalindrome("aacdefcaa"))
+        self.assertEqual("aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa", sol.longestPalindrome("aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa"))
