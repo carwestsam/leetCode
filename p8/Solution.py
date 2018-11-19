0624b524b47d8ef705ba235a855b9ea6be6dc27e
Cedric Sung <carwestsam@gmail.com>
2018-11-19 17:55:17 +0800

    P8: pure number

--- p8/Solution.py
+++ p8/Solution.py
@@ -4,4 +4,25 @@ class Solution:
         :type str: str
         :rtype: int
         """
-        return 0
+
+        i = 0
+        ans = 0
+
+        atoi_dict = {
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "0": 0
+        }
+
+        while i < len(str):
+            ans = ans * 10 + atoi_dict[str[i]]
+            i += 1
+
+        return ans
--- p8/test_solution.py
+++ p8/test_solution.py
@@ -8,3 +8,12 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(0, sol.myAtoi("0"))
+
+    def test_positive_number_without_white_space(self):
+        sol = Solution()
+
+        self.assertEqual(1, sol.myAtoi("1"))
+        self.assertEqual(12, sol.myAtoi("12"))
+        self.assertEqual(2147483647, sol.myAtoi("2147483647"))
+        self.assertEqual(2147483647, sol.myAtoi("2147483647"))
+
