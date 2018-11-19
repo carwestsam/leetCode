b7a2081e55c0d22b2bf7c882f3dd27bef00acfa0
Cedric Sung <carwestsam@gmail.com>
2018-11-19 19:11:45 +0800

    P8: fix bug

--- p8/Solution.py
+++ p8/Solution.py
@@ -7,8 +7,11 @@ class Solution:
 
         i = 0
         ans = 0
-        symbol = True
+        symbol = 1
         is_last_symbol = False
+        is_last_whitespace = True
+        is_number_read = False
+        valid = True
 
         atoi_dict = {
             "1": 1,
@@ -27,15 +30,35 @@ class Solution:
             "-": -1
         }
 
-        while i < len(str):
+        while i < len(str) and valid is True:
+            if str[i] in symbols and is_last_symbol is True:
+                valid = False
+                break
+            if is_number_read is True and str[i] not in atoi_dict:
+                break
             if str[i] in symbols:
                 is_last_symbol = True
+            elif is_last_symbol is True and str[i] not in atoi_dict:
+                is_last_symbol = False
+                valid = False
+            elif str[i] != " " and is_last_whitespace is True and is_number_read is False and str[i] not in symbols and str[i] not in atoi_dict:
+                valid = False
+                break
             elif str[i] in atoi_dict:
                 if is_last_symbol is True:
                     is_last_symbol = False
                     symbol = symbols[str[i-1]]
                 ans = ans * 10 + atoi_dict[str[i]]
+                is_number_read = True
+
+            if str[i] == " ":
+                is_last_whitespace = True
+            else:
+                is_last_whitespace = False
             i += 1
+
+        if valid is False:
+            return 0
 
         INT_MAX = 2147483647
         INT_MIN = -2147483648
--- p8/test_solution.py
+++ p8/test_solution.py
@@ -13,7 +13,7 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(1, sol.myAtoi("1"))
-        self.assertEqual(12, sol.myAtoi("12"))
+        self.assertEqual(12, sol.myAtoi("+12"))
         self.assertEqual(2147483647, sol.myAtoi("2147483647"))
         self.assertEqual(-2147483648, sol.myAtoi("-2147483648"))
 
@@ -35,3 +35,14 @@ class TestSolution(TestCase):
 
         self.assertEqual(-2147483648, sol.myAtoi("-91283472332"))
         self.assertEqual(2147483647, sol.myAtoi("91283472332"))
+
+    def test_return_zero_when_non_digit_at_begin(self):
+        sol = Solution()
+
+        self.assertEqual(0, sol.myAtoi(""))
+        self.assertEqual(0, sol.myAtoi("- -2"))
+        self.assertEqual(0, sol.myAtoi("word 2"))
+        self.assertEqual(2, sol.myAtoi("2 3"))
+        self.assertEqual(0, sol.myAtoi("words and 987"))
+        self.assertEqual(0, sol.myAtoi("- 2"))
+        self.assertEqual(0, sol.myAtoi("0-1"))
