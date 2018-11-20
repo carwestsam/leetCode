0455921c06dde867b7adb2d18e00f2fa11df191b
Cedric Sung <carwestsam@gmail.com>
2018-11-20 23:35:25 +0800

    P10: tokenize & start

--- p10/Solution.py
+++ p10/Solution.py
@@ -6,15 +6,64 @@ class Solution:
         :rtype: bool
         """
 
-        ans = True
-
-        if len(s) != len(p):
-            return False
-
-        for i in range(len(s)):
-            if p[i] == ".":
-                continue
-            elif p[i] != s[i]:
+        if "*" not in p:
+            if len(s) != len(p):
                 return False
 
-        return True
+            for i in range(len(s)):
+                if p[i] == ".":
+                    continue
+                elif p[i] != s[i]:
+                    return False
+
+            return True
+        else:
+            tokens = self.tokenize(p)
+            return self.matchable(s, tokens)
+
+    def matchable(self, origin, tokens):
+        current = {0: True}
+        nxt = {}
+        lo = len(origin)
+
+        for token in tokens:
+            if token == ".":
+                for pt in current:
+                    if pt < lo:
+                        nxt[pt + 1] = True
+
+            elif len(token) == 2:
+                for pt in current:
+                    nxt[pt] = True
+                    ipt = pt
+                    ch = token[0]
+                    while ipt < lo and (ch == '.' or origin[ipt] == ch):
+                        nxt[ipt+1] = True
+                        ipt += 1
+            else:
+                for pt in current:
+                    if pt < lo and origin[pt] == token :
+                        nxt[pt + 1] = True
+
+            current = nxt
+            nxt = {}
+
+        return lo in current
+
+    def tokenize(self, pattern):
+        tokens = []
+        i = 0
+        lp = len(pattern)
+
+        while i < lp:
+            if i + 1 < lp and pattern[i + 1] == '*':
+                tokens.append(pattern[i:i + 2])
+                i += 1
+            elif pattern[i] == '.':
+                tokens.append('.')
+            else:
+                tokens.append(pattern[i])
+
+            i += 1
+
+        return tokens
--- p10/test_solution.py
+++ p10/test_solution.py
@@ -30,3 +30,29 @@ class TestSolution(TestCase):
         self.assertEqual(False, sol.isMatch("dd", "."))
         self.assertEqual(False, sol.isMatch("", "."))
         self.assertEqual(False, sol.isMatch("abc", "a.d"))
+
+    def test_should_tokenize(self):
+        sol = Solution()
+
+        self.assertEqual(["a", ".", "b*"], sol.tokenize("a.b*"))
+        self.assertEqual(["a*", "b*", "c"], sol.tokenize("a*b*c"))
+        self.assertEqual(["b", ".*", "a"], sol.tokenize("b.*a"))
+
+    def test_isMatch_if_have_star(self):
+        sol = Solution()
+
+        self.assertEqual(True, sol.isMatch("aaaa", "a*"))
+        self.assertEqual(True, sol.isMatch("aaaab", "a*b"))
+        self.assertEqual(True, sol.isMatch("aab", "c*a*b"))
+
+        self.assertEqual(False, sol.isMatch("abcc", "ac*b"))
+        self.assertEqual(False, sol.isMatch("mississippi", "mis*is*p*."))
+
+    def test_match_if_dot_start(self):
+        sol = Solution()
+
+        self.assertEqual(True, sol.isMatch("ab", ".*"))
+        self.assertEqual(True, sol.isMatch("abc", ".*"))
+
+        self.assertEqual(False, sol.isMatch("dab", "c.*"))
+
