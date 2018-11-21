2b2db1d05e12bb60e039fcff5eef24d1dfae6d56
Cedric Sung <carwestsam@gmail.com>
2018-11-21 20:51:59 +0800

    P11: accepted, sorted by (a_i, i), then calculate maximum

--- p11/Solution.py
+++ p11/Solution.py
@@ -4,4 +4,34 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
-        return 1
+        pairs = []
+        for i in range(len(height)):
+            pairs.append((height[i], i + 1))
+
+        def getKey(elem):
+            return elem[0] * 100000000 + elem[1]
+
+        pairs.sort(key=getKey, reverse=True)
+
+        min_idx = pairs[0][1]
+        max_idx = pairs[0][1]
+        maximum = 0
+        for i in range(1, len(height)):
+            value, idx = pairs[i]
+            # left
+            if min_idx < idx:
+                v = (idx - min_idx) * value
+                if v > maximum:
+                    maximum = v
+            else:
+                min_idx = idx
+
+            # right
+            if max_idx > idx:
+                v = (max_idx - idx) * value
+
+                if v > maximum:
+                    maximum = v
+            else:
+                max_idx = idx
+        return maximum
--- p11/test_solution.py
+++ p11/test_solution.py
@@ -7,3 +7,4 @@ class TestSolution(TestCase):
     def test_maxArea(self):
         sol = Solution()
         self.assertEqual(1, sol.maxArea([1, 1]))
+        self.assertEqual(49, sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
