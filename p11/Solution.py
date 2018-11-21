2a5c3a5017abc66388642c4e6685e639f1920fce
Cedric Sung <carwestsam@gmail.com>
2018-11-21 20:56:59 +0800

    P11: greedy solution

--- p11/Solution.py
+++ p11/Solution.py
@@ -4,34 +4,17 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
-        pairs = []
-        for i in range(len(height)):
-            pairs.append((height[i], i + 1))
 
-        def getKey(elem):
-            return elem[0] * 100000000 + elem[1]
-
-        pairs.sort(key=getKey, reverse=True)
-
-        min_idx = pairs[0][1]
-        max_idx = pairs[0][1]
         maximum = 0
-        for i in range(1, len(height)):
-            value, idx = pairs[i]
-            # left
-            if min_idx < idx:
-                v = (idx - min_idx) * value
-                if v > maximum:
-                    maximum = v
-            else:
-                min_idx = idx
+        left = 0
+        right = len(height) - 1
 
-            # right
-            if max_idx > idx:
-                v = (max_idx - idx) * value
-
-                if v > maximum:
-                    maximum = v
+        while left < right:
+            if min(height[left], height[right]) * (right - left) > maximum:
+                maximum = min(height[left], height[right]) * (right - left)
+            if height[left] > height[right]:
+                right -= 1
             else:
-                max_idx = idx
+                left += 1
+
         return maximum
