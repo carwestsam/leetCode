3c6f8c2c56fdb483ffded8b2233e977dd6385745
Cedric Sung <carwestsam@gmail.com>
2018-11-22 14:40:18 +0800

    P12: on the chain

--- p12/Solution.py
+++ p12/Solution.py
@@ -5,19 +5,21 @@ class Solution:
         :rtype: str
         """
 
-        if num == 1000:
-            return "M"
-        elif num == 500:
-            return "D"
-        elif num == 100:
-            return "C"
-        elif num == 50:
-            return "L"
-        elif num == 10:
-            return "X"
-        elif num == 5:
-            return "V"
-        elif num == 1:
-            return "I"
+        ans = ""
+
+        if num >= 1000:
+            return "M" + self.intToRoman(num - 1000)
+        elif num >= 500:
+            return "D" + self.intToRoman(num - 500)
+        elif num >= 100:
+            return "C" + self.intToRoman(num - 100)
+        elif num >= 50:
+            return "L" + self.intToRoman(num - 50)
+        elif num >= 10:
+            return "X" + self.intToRoman(num - 10)
+        elif num >= 5:
+            return "V" + self.intToRoman(num - 5)
+        elif 0 <= num < 4:
+            return "I" * num
 
         return "I"
--- p12/test_solution.py
+++ p12/test_solution.py
@@ -19,3 +19,13 @@ class TestSolution(TestCase):
         self.assertEqual("C", sol.intToRoman(100))
         self.assertEqual("D", sol.intToRoman(500))
         self.assertEqual("M", sol.intToRoman(1000))
+
+    def test_should_handle_chain(self):
+        sol = Solution()
+
+        self.assertEqual("III", sol.intToRoman(3))
+        self.assertEqual("VIII", sol.intToRoman(8))
+        self.assertEqual("XIII", sol.intToRoman(13))
+        self.assertEqual("XVIII", sol.intToRoman(18))
+        self.assertEqual("MDCLXVI", sol.intToRoman(1666))
+        self.assertEqual("MXVIII", sol.intToRoman(1018))
