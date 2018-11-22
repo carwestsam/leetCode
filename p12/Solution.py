35294bf1705d4758a5a8f30eba33ee340ff7bdb6
Cedric Sung <carwestsam@gmail.com>
2018-11-22 14:29:04 +0800

    P12: one char

--- p12/Solution.py
+++ p12/Solution.py
@@ -4,4 +4,20 @@ class Solution:
         :type num: int
         :rtype: str
         """
+
+        if num == 1000:
+            return "M"
+        elif num == 500:
+            return "D"
+        elif num == 100:
+            return "C"
+        elif num == 50:
+            return "L"
+        elif num == 10:
+            return "X"
+        elif num == 5:
+            return "V"
+        elif num == 1:
+            return "I"
+
         return "I"
--- p12/test_solution.py
+++ p12/test_solution.py
@@ -8,3 +8,14 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual("I", sol.intToRoman(1))
+
+    def test_should_handle_one_char_digit(self):
+        sol = Solution()
+
+        self.assertEqual("I", sol.intToRoman(1))
+        self.assertEqual("V", sol.intToRoman(5))
+        self.assertEqual("X", sol.intToRoman(10))
+        self.assertEqual("L", sol.intToRoman(50))
+        self.assertEqual("C", sol.intToRoman(100))
+        self.assertEqual("D", sol.intToRoman(500))
+        self.assertEqual("M", sol.intToRoman(1000))
