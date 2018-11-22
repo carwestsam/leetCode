0200d8a7636834ea3f0564fa8eabce376cc8f00b
Cedric Sung <carwestsam@gmail.com>
2018-11-22 15:21:59 +0800

    P13: single char

--- p13/Solution.py
+++ p13/Solution.py
@@ -4,4 +4,36 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        return 1
+        ans = 0
+        i = 0
+        s = s + "##"
+
+        while s[i] == 'M':
+            ans += 1000
+            i += 1
+
+        if s[i] == 'D':
+            ans += 500
+            i += 1
+
+        if s[i] == 'C':
+            ans += 100
+            i += 1
+
+        if s[i] == 'L':
+            ans += 50
+            i += 1
+
+        if s[i] == 'X':
+            ans += 10
+            i += 1
+
+        if s[i] == 'V':
+            ans += 5
+            i += 1
+
+        if s[i] == 'I':
+            ans += 1
+            i += 1
+
+        return ans
--- p13/test_solution.py
+++ p13/test_solution.py
@@ -8,3 +8,13 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(1, sol.romanToInt("I"))
+
+    def test_single_char(self):
+        sol = Solution()
+
+        self.assertEqual(5, sol.romanToInt("V"))
+        self.assertEqual(10, sol.romanToInt("X"))
+        self.assertEqual(50, sol.romanToInt("L"))
+        self.assertEqual(100, sol.romanToInt("C"))
+        self.assertEqual(500, sol.romanToInt("D"))
+        self.assertEqual(1000, sol.romanToInt("M"))
