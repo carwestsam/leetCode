ce105684bde9f53b3ce4c1382dc0bdd49dc6dfaf
Cedric Sung <carwestsam@gmail.com>
2018-11-25 18:01:04 +0800

    P15: accepted

--- p15/Solution.py
+++ p15/Solution.py
@@ -4,5 +4,34 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
 
-        return [[-1, -2, 3]]
+        ans = []
+        # print('sorted', nums)
+
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s < 0:
+                    j += 1
+                elif s == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    k -= 1
+                    j += 1
+                elif s > 0:
+                    k -= 1
+
+        if len(ans) < 2:
+            return ans
+
+        fans = [ans[0]]
+
+        for i in range(1, len(ans)):
+            if ans[i] != ans[i-1]:
+                fans.append(ans[i])
+
+        return fans
--- p15/test_solution.py
+++ p15/test_solution.py
@@ -6,4 +6,10 @@ from p15.Solution import Solution
 class TestSolution(TestCase):
     def test_threeSum(self):
         sol = Solution()
-        self.assertEqual([[-1, -2, 3]], sol.threeSum([-1, -2, 3]))
+        self.assertEqual([[-2, -1, 3]], sol.threeSum([-1, -2, 3]))
+
+        self.assertEqual([[-1, -1, 2], [-1, 0, 1]], sol.threeSum([-1, 0, 1, 2, -1, -4]))
+        self.assertEqual([[-2, 0, 2], [-1, 0, 1]], sol.threeSum([-2, -1, 0, 1, 2]))
+        self.assertEqual([[-1, 0, 1]], sol.threeSum([-1, 0, 1, 0]))
+        self.assertEqual([[-4, -2, 6], [-4, 0, 4], [-4, 1, 3], [-4, 2, 2], [-2, -2, 4], [-2, 0, 2]],
+                         sol.threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
