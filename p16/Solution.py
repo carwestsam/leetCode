16b97c8875e37a1f2747ec7fd0bb3e6edce72344
Cedric Sung <carwestsam@gmail.com>
2018-11-26 23:25:48 +0800

    P16: target out of range

--- p16/Solution.py
+++ p16/Solution.py
@@ -5,4 +5,17 @@ class Solution:
         :type target: int
         :rtype: int
         """
+
+        nums.sort()
+
+        print(nums)
+
+        left = 0
+        right = len(nums) - 1
+
+        if target <= nums[left]:
+            return [nums[0], nums[1], nums[2]]
+        elif target >= nums[right]:
+            return [nums[-3], nums[-2], nums[-1]]
+
         return [0, 0, 0]
--- p16/test_solution.py
+++ p16/test_solution.py
@@ -8,3 +8,6 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual([0, 0, 0], sol.threeSumClosest([0, 0, 0], 1))
+        self.assertEqual([-4, -1, 1], sol.threeSumClosest([-1, 2, 1, -4], -5))
+        self.assertEqual([-1, 1, 2], sol.threeSumClosest([-1, 2, 1, -4], 3))
+        self.assertEqual([-1, 1, 2], sol.threeSumClosest([-1, 2, 1, -4], 3))
