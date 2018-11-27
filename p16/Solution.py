10cd5be504df26f9a8cd047b85f96933fada71b8
Cedric Sung <carwestsam@gmail.com>
2018-11-27 23:34:11 +0800

    P16: wrong answer

--- p16/Solution.py
+++ p16/Solution.py
@@ -8,14 +8,29 @@ class Solution:
 
         nums.sort()
 
-        print(nums)
+        # print(nums)
 
         left = 0
         right = len(nums) - 1
 
         if target <= nums[left]:
-            return [nums[0], nums[1], nums[2]]
+            return sum([nums[0], nums[1], nums[2]])
         elif target >= nums[right]:
-            return [nums[-3], nums[-2], nums[-1]]
+            return sum([nums[-3], nums[-2], nums[-1]])
 
-        return [0, 0, 0]
+        def abs(v):
+            if v < 0:
+                return -v
+            else:
+                return v
+
+        while left + 2 < right:
+            dl = abs(nums[left] - target)
+            dr = abs(nums[right] - target)
+
+            if dl >= dr:
+                left = left + 1
+            elif dr > dl:
+                right = right - 1
+
+        return sum(nums[left: right + 1])
--- p16/test_solution.py
+++ p16/test_solution.py
@@ -7,7 +7,9 @@ class TestSolution(TestCase):
     def test_threeSumClosest(self):
         sol = Solution()
 
-        self.assertEqual([0, 0, 0], sol.threeSumClosest([0, 0, 0], 1))
-        self.assertEqual([-4, -1, 1], sol.threeSumClosest([-1, 2, 1, -4], -5))
-        self.assertEqual([-1, 1, 2], sol.threeSumClosest([-1, 2, 1, -4], 3))
-        self.assertEqual([-1, 1, 2], sol.threeSumClosest([-1, 2, 1, -4], 3))
+        self.assertEqual(0, sol.threeSumClosest([0, 0, 0], 1))
+        self.assertEqual(-4, sol.threeSumClosest([-1, 2, 1, -4], -5))
+        self.assertEqual(2, sol.threeSumClosest([-1, 2, 1, -4], 3))
+        self.assertEqual(2, sol.threeSumClosest([-1, 2, 1, -4], 3))
+        self.assertEqual(2, sol.threeSumClosest([-1, 2, 1, -4], 1))
+        # self.assertEqual(0, sol.threeSumClosest([0, 2, 1, -3], 1))
