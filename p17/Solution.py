a6e28587d6ff5c7c02c1e33b37dbfb352598e7af
Cedric Sung <carwestsam@gmail.com>
2018-11-29 23:26:00 +0800

    P17: single digit

--- p17/Solution.py
+++ p17/Solution.py
@@ -4,4 +4,21 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        return ["a", "b", "c"]
+        num2char = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
+        }
+        lnc = {}
+        for k in num2char:
+            lnc[k] = num2char[k].__len__()
+
+        ans = []
+        for j in range(lnc[digits[0]]):
+            ans.append(num2char[digits[0]][j])
+        return ans
--- p17/test_solution.py
+++ p17/test_solution.py
@@ -8,3 +8,10 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(["a", "b", "c"], sol.letterCombinations("2"))
+        self.assertEqual(["d", "e", "f"], sol.letterCombinations("3"))
+        self.assertEqual(["g", "h", "i"], sol.letterCombinations("4"))
+        self.assertEqual(["j", "k", "l"], sol.letterCombinations("5"))
+        self.assertEqual(["m", "n", "o"], sol.letterCombinations("6"))
+        self.assertEqual(["p", "q", "r", 's'], sol.letterCombinations("7"))
+        self.assertEqual(["t", "u", "v"], sol.letterCombinations("8"))
+        self.assertEqual(["w", "x", "y", "z"], sol.letterCombinations("9"))
