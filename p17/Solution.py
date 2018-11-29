46508b0b2126d684c797f77621ea2be22f3e4f3d
Cedric Sung <carwestsam@gmail.com>
2018-11-30 00:06:11 +0800

    P17: 1 key && empty case && accepted && faster then 100%!

--- p17/Solution.py
+++ p17/Solution.py
@@ -4,10 +4,14 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        if len(digits) == 0:
+        ld = len(digits)
+        if ld == 0:
+            return []
+        if ld == 1 and digits[0] == '1':
             return []
 
         num2char = {
+            '1': '',
             '2': "abc",
             '3': "def",
             '4': "ghi",
@@ -18,10 +22,49 @@ class Solution:
             '9': "wxyz"
         }
         lnc = {}
+
         for k in num2char:
             lnc[k] = num2char[k].__len__()
 
         ans = []
-        for j in range(lnc[digits[0]]):
-            ans.append(num2char[digits[0]][j])
+
+        stack = [0] * (ld + 1)
+        stackStr = [""] * (ld + 1)
+        top = 0
+
+        while top >= 0:
+            if top == ld:
+                if stackStr[top - 1] != '':
+                    ans.append(stackStr[top - 1])
+                top -= 1
+                stack[top] += 1
+                continue
+
+            if top == 0:
+                if digits[top] == '1' and stack[top] == 0:
+                    stackStr[0] = ''
+                    top += 1
+                    stack[top] = 0
+                elif stack[top] >= lnc[digits[0]]:
+                    top -= 1
+                    stack[top] += 1
+                else:
+                    # print(top, digits[0], stack[top])
+                    stackStr[0] = num2char[digits[0]][stack[top]]
+                    top += 1
+                    stack[top] = 0
+                continue
+
+            if digits[top] == '1' and stack[top] == 0:
+                stackStr[top] = stackStr[top - 1]
+                top += 1
+                stack[top] = 0
+            elif stack[top] >= lnc[digits[top]]:
+                top -= 1
+                stack[top] += 1
+            else:
+                stackStr[top] = stackStr[top-1] + num2char[digits[top]][stack[top]]
+                top += 1
+                stack[top] = 0
+
         return ans
--- p17/test_solution.py
+++ p17/test_solution.py
@@ -17,3 +17,22 @@ class TestSolution(TestCase):
         self.assertEqual(["p", "q", "r", 's'], sol.letterCombinations("7"))
         self.assertEqual(["t", "u", "v"], sol.letterCombinations("8"))
         self.assertEqual(["w", "x", "y", "z"], sol.letterCombinations("9"))
+
+    def test_handle_double_or_more_numbers(self):
+        sol = Solution()
+
+        self.assertEqual(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], sol.letterCombinations("23"))
+        tmp1 = ["adg", "aeg", "afg", "bdg", "beg", "bfg", "cdg", "ceg", "cfg",
+                "adh", "aeh", "afh", "bdh", "beh", "bfh", "cdh", "ceh", "cfh",
+                "adi", "aei", "afi", "bdi", "bei", "bfi", "cdi", "cei", "cfi"]
+        print(tmp1.sort())
+        self.assertEqual(tmp1, sol.letterCombinations("234"))
+
+    def test_should_handle_key_1(self):
+        sol = Solution()
+        self.assertEqual([], sol.letterCombinations("1"))
+        self.assertEqual([], sol.letterCombinations("111"))
+        self.assertEqual(['a', 'b', 'c'], sol.letterCombinations("12"))
+        self.assertEqual(['a', 'b', 'c'], sol.letterCombinations("21"))
+        self.assertEqual(['a', 'b', 'c'], sol.letterCombinations("11211"))
+        self.assertEqual(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], sol.letterCombinations("11211311"))
