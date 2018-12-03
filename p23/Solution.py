829e182d83fd12bde496aca03ebae06a66b5c7db
Cedric Sung <carwestsam@gmail.com>
2018-12-03 10:02:42 +0800

    P23: heap down

--- p23/Solution.py
+++ p23/Solution.py
@@ -5,63 +5,57 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
-    def swap(self, lists, a, b):
-        t = lists[a]
-        lists[a] = lists[b]
-        lists[b] = t
-
-    def down(self, lists, idx):
-        size = len(lists)
-        l = (idx+1) * 2 - 1
-        r = l + 1
-        if l < size:
-            t = l
-            if r < size and lists[l].val > lists[r].val:
-                t = r
-
-            if lists[t].val < lists[idx].val:
-                self.swap(lists, t, idx)
-                self.down(lists, t)
-
-    def mergeKLists(self, olists):
+    def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-
-        lists = []
-
-        for node in olists:
-            if node != None:
-                lists.append(node)
-
         if len(lists) == 0:
             return None
+        elif len(lists) == 1:
+            return lists[0]
 
-        for x in range(len(lists)//2, -1, -1):
-            self.down(lists, x)
+    def heapSwap(self, left, right, heap):
+        result = heap[left]
+        heap[left] = heap[right]
+        heap[right] = result
 
-        ans = []
+    def heapInit(self, heap):
+        lo = len(heap)
+        for i in range(lo - 1, -1, -1):
+            self.heapDown(i, heap)
+        return heap
 
-        while len(lists) > 0:
-            ans.append(lists[0])
+    def heapDown(self, x, heap):
+        target = (x + 1) * 2 - 1
 
-            if lists[0].next == None:
-                if len(lists) > 1:
-                    lists[0] = lists[len(lists) - 1]
-                    del lists[len(lists)-1]
-                    self.down(lists, 0)
-                else :
-                    del lists[0]
+        lh = len(heap)
 
-            else :
-                lists[0] = lists[0].next
-                self.down(lists, 0)
+        if target >= lh:
+            return
 
-        for x in range(len(ans) - 1):
-            ans[x].next = ans[x+1]
+        if target + 1 < lh and heap[target + 1] < heap[target]:
+            target += 1
 
-        ans[len(ans) -1].next = None
+        if heap[target] < heap[x]:
+            self.heapSwap(x, target, heap)
+            self.heapDown(target, heap)
 
-        return ans[0]
+    def heapPop(self, heap):
+        lo = len(heap)
+
+        if lo == 0:
+            return None
+
+        result = heap[0]
+
+        if lo == 1:
+            heap.pop()
+            return result
+
+        v = heap.pop()
+        heap[0] = v
+        self.heapDown(0, heap)
+        return result
--- p23/test_solution.py
+++ p23/test_solution.py
@@ -1,38 +1,51 @@
 from unittest import TestCase
 from p23.Solution import Solution, ListNode
+from random import randint
+
 
 class TestSolution(TestCase):
-  def test_should_return_origin_list(self):
-    sol = Solution()
-    n1 = ListNode(1)
-    n2 = ListNode(2)
-    n3 = ListNode(3)
-    n1.next = n2
-    n2.next = n3
+    def test_should_init_and_pop_a_minimum_heap(self):
+        sol = Solution()
+        origin = [randint(0, 1000) for _ in range(20)]
+        expected = sorted(origin)
+        sol.heapInit(origin)
+        self.assertEqual(min(origin), origin[0])
+        actual = []
+        for i in range(20):
+            actual.append(sol.heapPop(origin))
+        self.assertEqual(expected, actual)
 
-    ans = sol.mergeKLists([n1])
+    def xtest_should_return_origin_list(self):
+        sol = Solution()
+        n1 = ListNode(1)
+        n2 = ListNode(2)
+        n3 = ListNode(3)
+        n1.next = n2
+        n2.next = n3
 
-    self.assertEqual(ans, n1)
+        ans = sol.mergeKLists([n1])
 
-  def test_should_return_merged_list(self):
-    sol = Solution()
-    n11 = ListNode(1)
-    n12 = ListNode(3)
-    n11.next = n12
+        self.assertEqual(ans, n1)
 
-    n21 = ListNode(2)
-    n22 = ListNode(6)
-    n21.next = n22
+    def test_should_return_merged_list(self):
+        sol = Solution()
+        n11 = ListNode(1)
+        n12 = ListNode(3)
+        n11.next = n12
 
-    n31 = ListNode(4)
-    n32 = ListNode(5)
-    n31.next = n32
+        n21 = ListNode(2)
+        n22 = ListNode(6)
+        n21.next = n22
 
-    ans = sol.mergeKLists([n21, n31, n11])
+        n31 = ListNode(4)
+        n32 = ListNode(5)
+        n31.next = n32
 
-    self.assertEqual(ans, n11)
+        ans = sol.mergeKLists([n21, n31, n11])
 
-  def test_special(self):
-    sol = Solution()
-    ans = sol.mergeKLists([None])
-    self.assertEqual(ans, None)
+        self.assertEqual(ans, n11)
+
+    def test_special(self):
+        sol = Solution()
+        ans = sol.mergeKLists([None])
+        self.assertEqual(ans, None)
