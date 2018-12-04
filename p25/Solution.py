8b2dfec6a93b0dc2ae27212a7fc52136ec780349
Cedric Sung <carwestsam@gmail.com>
2018-12-04 10:41:45 +0800

    p25: should not revserse tail sequense

--- p25/Solution.py
+++ p25/Solution.py
@@ -11,6 +11,62 @@ class Solution:
         :type k: int
         :rtype: ListNode
         """
+        if head is None:
+            return None
         if k == 1:
             return head
-        return None
+
+        begin = head
+        end = head
+        pt = None
+
+        def reversable(h, k):
+            p = h
+            for _ in range(k - 1):
+                p = p.next
+                if p is None:
+                    return False
+            return p is not None
+
+        if not reversable(head, k):
+            return head
+
+        for i in range(k):
+            tmp = end
+            end = end.next
+
+            if i != k - 1 and end is None:
+                return head
+
+            if pt is None:
+                pt = tmp
+            else:
+                tmp.next = pt
+                pt = tmp
+
+        head = pt
+        begin.next = end
+
+        while end is not None:
+            prehead = begin
+            begin = end
+            pt = None
+            if not reversable(begin, k):
+                return head
+            for i in range(k):
+                tmp = end
+                end = end.next
+
+                if i != k - 1 and end is None:
+                    return head
+
+                if pt is None:
+                    pt = tmp
+                else:
+                    tmp.next = pt
+                    pt = tmp
+
+            prehead.next = pt
+            begin.next = end
+
+        return head
--- p25/test_solution.py
+++ p25/test_solution.py
@@ -15,3 +15,23 @@ class TestSolution(TestCase):
 
         self.assertEqual([1, 2, 3, 4, 5], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5]), 1)))
         self.assertEqual([1, 2, 3], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3]), 1)))
+
+    def test_should_return_K_reversed_nodes(self):
+        sol = Solution()
+
+        self.assertEqual([1], nodes_to_list(sol.reverseKGroup(list_to_nodes([1]), 2)))
+        self.assertEqual([2, 1], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2]), 2)))
+        self.assertEqual([2, 1, 4, 3, 5], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5]), 2)))
+        self.assertEqual([2, 1, 4, 3, 6, 5], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5, 6]), 2)))
+        self.assertEqual([3, 2, 1, 4, 5], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5]), 3)))
+        self.assertEqual([3, 2, 1, 6, 5, 4, 7],
+                         nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5, 6, 7]), 3)))
+
+    def test_should_fix_error_case(self):
+        sol = Solution()
+
+        self.assertEqual([1, 2, 3], nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3]), 4)))
+        self.assertEqual([4, 3, 2, 1, 5, 6, 7],
+                         nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5, 6, 7]), 4)))
+        self.assertEqual([4, 3, 2, 1, 8, 7, 6, 5, 9, 10],
+                         nodes_to_list(sol.reverseKGroup(list_to_nodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 4)))
