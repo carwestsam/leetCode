41316f6262c3fa84a1e6dabfa037ec763c7306b1
Cedric Sung <carwestsam@gmail.com>
2018-12-05 10:51:52 +0800

    p26: origin length less than two

--- p26/test_solution.py
+++ p26/test_solution.py
@@ -4,7 +4,8 @@ from p26.Solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_removeDuplicates(self):
+    def test_removeDuplicates_origin_length_less_than_2(self):
         sol = Solution()
 
         self.assertEqual(1, sol.removeDuplicates([1]))
+        self.assertEqual(0, sol.removeDuplicates([]))
