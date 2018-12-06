72b1eb9abeae1038c05e3631ba09f1f9a436a843
Cedric Sung <carwestsam@gmail.com>
2018-12-07 00:02:04 +0800

    p28: KMP

--- p28/Solution.py
+++ p28/Solution.py
@@ -5,4 +5,44 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        ln = len(needle)
+        lh = len(haystack)
+
+        if ln == 0:
+            return 0
+
+        prefix = [-1] * ln
+
+        i = 1
+        j = -1
+
+        while i < ln:
+            while j != -1  and needle[i] != needle[j + 1]:
+                j = prefix[j]
+
+            if j < ln - 1 and needle[i] == needle[j + 1]:
+                prefix[i] = j + 1
+                j += 1
+            else:
+                prefix[i] = -1
+
+            i += 1
+
+        # print(prefix)
+
+
+        pn = -1
+
+        for ph in range(lh):
+            ch = haystack[ph]
+            # print(pn + 1)
+            while pn != -1 and pn < ln - 1 and ch != needle[pn + 1]:
+                pn = prefix[pn]
+
+            if ch == needle[pn + 1]:
+                pn = pn + 1
+
+            if pn + 1 == ln:
+                return ph - pn
+
         return -1
--- p28/test_solution.py
+++ p28/test_solution.py
@@ -8,3 +8,20 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(-1, sol.strStr("hello", "aa"))
+
+    def test_should_return_zero_when_needle_is_empty(self):
+        sol = Solution()
+        self.assertEqual(0, sol.strStr("hello", ""))
+
+    def test_should_return_zero_when_plant_is_true(self):
+        sol = Solution()
+        self.assertEqual(1, sol.strStr("aabababa", "abababa"))
+        self.assertEqual(2, sol.strStr("Hello", "ll"))
+        self.assertEqual(-1, sol.strStr("aaaaa", "bba"))
+        self.assertEqual(1, sol.strStr("Hello", "ello"))
+
+    def test_wrong_case(self):
+        sol = Solution()
+
+        self.assertEqual(4, sol.strStr("mississippi", "issip"))
+        self.assertEqual(0, sol.strStr("a", "a"))
