dcbff9ee9630b9b2fe3e78c2949629a10ce4b5e7
Cedric Sung <carwestsam@gmail.com>
2018-12-08 21:00:43 +0800

    p29: divide

--- p29/Solution.py
+++ p29/Solution.py
@@ -6,15 +6,40 @@ class Solution:
         :rtype: int
         """
 
+        rev_dividend = False
+        rev_divisor = False
+
+        if divisor < 0:
+            rev_divisor = True
+            divisor = - divisor
+
+        if dividend < 0:
+            rev_dividend = True
+            dividend = - dividend
+
         remains = dividend
         multi = divisor
+        ans = 0
+        multi_val = 1
         while multi <= dividend:
             multi <<= 1
+            multi_val <<= 1
+
         multi >>= 1
+        multi_val >>= 1
 
         while remains >= divisor:
             if remains >= multi:
                 remains -= multi
+                ans += multi_val
+
+            multi_val >>= 1
             multi >>= 1
 
-        return remains
+        if rev_dividend is True and rev_divisor is False and remains != 0:
+            ans += 1
+
+        if rev_divisor != rev_dividend:
+            return - ans
+
+        return ans
--- p29/test_solution.py
+++ p29/test_solution.py
@@ -7,8 +7,17 @@ class TestSolution(TestCase):
     def test_divide(self):
         sol = Solution()
 
-        self.assertEqual(0, sol.divide(2, 2))
-        self.assertEqual(1, sol.divide(10, 3))
-        self.assertEqual(1, sol.divide(3001, 3))
-        self.assertEqual(75, sol.divide(3001, 77))
+        self.assertEqual(1, sol.divide(2, 2))
+        self.assertEqual(3, sol.divide(10, 3))
+        self.assertEqual(38, sol.divide(3001, 77))
+
+    def test_handle_negtive(self):
+        sol = Solution()
+
+        self.assertEqual(-2, sol.divide(7, -3))
+        self.assertEqual(-7, sol.divide(21, -3))
+        self.assertEqual(-8, sol.divide(-23, 3))
+        self.assertEqual(-8, sol.divide(-24, 3))
+        self.assertEqual(7, sol.divide(-23, -3))
+        self.assertEqual(44574, sol.divide(-3432213, -77))
 
