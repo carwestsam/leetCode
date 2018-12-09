abac294fe8cac3707163e8b884e3d4f392acbe95
Cedric Sung <carwestsam@gmail.com>
2018-12-10 00:55:52 +0800

    p31: swap in different cases

--- p31/Solution.py
+++ p31/Solution.py
@@ -4,4 +4,47 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        return nums
+        ln = len(nums)
+
+        if ln <= 1:
+            return
+
+        def swap (x, y):
+            t = nums[x]
+            nums[x] = nums[y]
+            nums[y] = t
+
+        i = ln - 2
+        while i >= 0:
+            if nums[i] >= nums[i+1]:
+                i = i - 1
+            else:
+                break
+
+        if i == -1:
+            l = 0
+            r = ln -1
+            while l < r:
+                swap(l, r)
+                l += 1
+                r -= 1
+        elif i == ln - 2:
+            swap(i, i+1)
+            return
+        else:
+            r = ln - 1
+            while r > i and nums[i] >= nums[r]:
+                r -= 1
+
+            if r > i + 1:
+                swap(r, i)
+            else:
+                swap(i, i+1)
+            l = i + 1
+            r = ln - 1
+            while l < r:
+                swap(l, r)
+                l +=1
+                r -= 1
+            # nums[i+1:ln].sort()
+
--- p31/test_solution.py
+++ p31/test_solution.py
@@ -7,4 +7,42 @@ class TestSolution(TestCase):
     def test_nextPermutation(self):
         sol = Solution()
         nums = [1]
-        self.assertEqual([1], sol.nextPermutation(nums))
+        sol.nextPermutation(nums)
+        self.assertEqual([1], nums)
+
+        nums = [3, 2, 1]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 2, 3], nums)
+
+        nums = [1, 2, 3]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 3, 2], nums)
+
+        nums = [1, 3, 2]
+        sol.nextPermutation(nums)
+        self.assertEqual([2, 1, 3], nums)
+
+        nums = [1, 1, 3, 2]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 2, 1, 3], nums)
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 2, 3, 1], nums)
+
+        nums = [1, 1, 1, 3, 1, 1]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 1, 3, 1, 1, 1], nums)
+
+    def test_others(self):
+        sol = Solution()
+
+        nums = [1, 1, 5]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 5, 1], nums)
+        sol.nextPermutation(nums)
+        self.assertEqual([5, 1, 1], nums)
+
+        nums = [1, 4, 2, 3]
+        sol.nextPermutation(nums)
+        self.assertEqual([1, 4, 3, 2], nums)
+        sol.nextPermutation(nums)
+        self.assertEqual([2, 1, 3, 4], nums)
