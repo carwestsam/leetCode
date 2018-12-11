a326b25c1b98eb70cdca7b09d7cd0e2301573442
Cedric Sung <carwestsam@gmail.com>
2018-12-11 23:28:53 +0800

    p33: binary search

--- p33/Solution.py
+++ p33/Solution.py
@@ -5,4 +5,37 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        return -1
+        return self.binary_search(nums, 0, len(nums), target)
+
+    def binary_search(self, nums, left, right, target):
+        if left >= right:
+            return -1
+        if nums[left] == target:
+            return left
+        if nums[left] < nums[right-1]:
+            mid = (left + right) // 2
+            vm = nums[mid]
+            if vm == target:
+                return mid
+            elif vm > target:
+                return self.binary_search(nums, left + 1, mid, target)
+            else:
+                return self.binary_search(nums, mid + 1, right, target)
+
+        elif nums[left] > nums[right-1]:
+            mid = (left + right) // 2
+            vm = nums[mid]
+            if vm == target:
+                return mid
+            elif vm > nums[left]:
+                if nums[left] <= target <= vm:
+                    return self.binary_search(nums, left + 1, mid, target)
+                else:
+                    return self.binary_search(nums, mid + 1, right, target)
+            elif vm < nums[left]:
+                if vm <= target <= nums[right - 1]:
+                    return self.binary_search(nums, mid + 1, right, target)
+                else:
+                    return self.binary_search(nums, left + 1, mid, target)
+        else:
+            return -1
--- p33/test_solution.py
+++ p33/test_solution.py
@@ -8,3 +8,9 @@ class TestSolution(TestCase):
         sol = Solution()
 
         self.assertEqual(-1, sol.search([], 1))
+        self.assertEqual(4, sol.search([4, 5, 6, 7, 0, 1, 2], 0))
+        self.assertEqual(-1, sol.search([4, 5, 6, 7, 0, 1, 2], 3))
+        self.assertEqual(1, sol.search([4, 5, 6, 7, 0, 1, 2], 5))
+        self.assertEqual(-1, sol.search([4], 5))
+        self.assertEqual(0, sol.search([4], 4))
+        self.assertEqual(-1, sol.search([1, 3], 0))
