1be95fa8c2a43b6e07e21a3b4d833b25e8871d5c
Cedric Sung <carwestsam@gmail.com>
2018-12-12 10:53:26 +0800

    p63 - DP solution

--- p63/Solution.py
+++ p63/Solution.py
@@ -5,4 +5,37 @@ class Solution:
         :rtype: int
         """
 
-        return 0
+        if len(obstacleGrid) == 0:
+            return 0
+
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        row = len(obstacleGrid)
+        col = len(obstacleGrid[0])
+
+        f = []
+
+        for i in range(row):
+            f.append([0] * col)
+
+        f[0][0] = 1
+
+        for i in range(1, row):
+            if obstacleGrid[i][0] == 0:
+                f[i][0] = 1
+            else:
+                break
+
+        for j in range(1, col):
+            if obstacleGrid[0][j] == 0:
+                f[0][j] = 1
+            else:
+                break
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if obstacleGrid[i][j] == 0:
+                    f[i][j] = f[i - 1][j] + f[i][j - 1]
+
+        return f[row - 1][col - 1]
--- p63/test_solution.py
+++ p63/test_solution.py
@@ -11,3 +11,33 @@ class TestSolution(TestCase):
             [0, 1, 0],
             [0, 1, 0]
         ]))
+        self.assertEqual(6, sol.uniquePathsWithObstacles([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]))
+
+        self.assertEqual(3, sol.uniquePathsWithObstacles([
+            [0, 0, 0],
+            [0, 0, 0]
+        ]))
+
+    def test_should_return_zero_when_no_grid(self):
+        sol = Solution()
+        self.assertEqual(0, sol.uniquePathsWithObstacles([]))
+
+    def test_should_return_example_case(self):
+        sol = Solution()
+        self.assertEqual(2, sol.uniquePathsWithObstacles([
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]))
+
+    def test_should_zero_if_start_point_is_obstacle(self):
+        sol = Solution()
+        self.assertEqual(0, sol.uniquePathsWithObstacles([
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]))
