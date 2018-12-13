d8c582b0bc5c162dcd15a0b2cbad09a7671980a4
Cedric Sung <carwestsam@gmail.com>
2018-12-13 09:59:04 +0800

    P307: init and calc sum

--- p307/Solution.py
+++ p307/Solution.py
@@ -4,6 +4,10 @@ class NumArray:
         """
         :type nums: List[int]
         """
+        self._length = len(nums)
+        self._tree = [0] * (self._length * 2 + 20)
+        self._nums = nums
+        self._init_tree(0, self._length - 1, 1)
 
     def update(self, i, val):
         """
@@ -19,4 +23,29 @@ class NumArray:
         :type j: int
         :rtype: int
         """
-        return 6
+        return self._sum_tree(i, j, 0, self._length - 1, 1)
+
+    def _init_tree(self, left, right, idx):
+        if left == right:
+            self._tree[idx] = self._nums[left]
+            return self._tree[idx]
+        elif left > right:
+            return 0
+        else:
+            mid = (left + right) // 2
+            left_arm = self._init_tree(left, mid, idx * 2)
+            right_arm = self._init_tree(mid + 1, right, idx * 2 + 1)
+            self._tree[idx] = left_arm + right_arm
+            return left_arm + right_arm
+
+    def _sum_tree(self, target_left, target_right, left, right, idx):
+        if left > right or right < target_left or target_right < left:
+            return 0
+
+        if target_left <= left and right <= target_right:
+            return self._tree[idx]
+
+        mid = (left + right) // 2
+        left_arm = self._sum_tree(target_left, target_right, left, mid, idx * 2)
+        right_arm = self._sum_tree(target_left, target_right, mid + 1, right, idx * 2 + 1)
+        return left_arm + right_arm
--- p307/test_numArray.py
+++ p307/test_numArray.py
@@ -11,3 +11,4 @@ class TestNumArray(TestCase):
     def test_sumRange(self):
         num_array = NumArray([1, 2, 3, 4, 5, 6, 7])
         self.assertEqual(6, num_array.sumRange(0, 2))
+        self.assertEqual(10, num_array.sumRange(0, 3))
