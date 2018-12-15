0550e7ac4ec70d6b25e56bdb63553dacd95bddbe
Cedric Sung <carwestsam@gmail.com>
2018-12-15 23:04:06 +0800

    P46: accepted

--- p46/Solution.py
+++ p46/Solution.py
@@ -4,4 +4,26 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        return []
+        ln = len(nums)
+        if ln == 0:
+            return []
+        ans = []
+        self.assemble(nums, 0, ln, ans)
+        return ans
+
+    def assemble(self, nums, idx, ln, ans):
+        if idx == ln:
+            tmp = [0] * ln
+            for i in range(ln):
+                tmp[i] = nums[i]
+            ans.append(tmp)
+            return
+
+        for i in range(idx, ln):
+            t = nums[i]
+            nums[i] = nums[idx]
+            nums[idx] = t
+            self.assemble(nums, idx + 1, ln, ans)
+            t = nums[i]
+            nums[i] = nums[idx]
+            nums[idx] = t
--- p46/test_solution.py
+++ p46/test_solution.py
@@ -7,3 +7,6 @@ class TestSolution(TestCase):
     def test_permute(self):
         sol = Solution()
         self.assertEqual([], sol.permute([]))
+        self.assertEqual([[1]], sol.permute([1]))
+        self.assertEqual([[1, 2], [2, 1]], sol.permute([1, 2]))
+        self.assertEqual([[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]], sol.permute([1, 2, 3]))
