418aaeb9743dd3f1684f987c51272ff89eb2857d
Cedric Sung <carwestsam@gmail.com>
2018-12-21 01:11:40 +0800

    p777: accepted

--- p777/test_solution.py
+++ p777/test_solution.py
@@ -7,7 +7,7 @@ class TestSolution(TestCase):
     def test_canTransform(self):
         sol = Solution()
         self.assertEqual(False, sol.canTransform("L", "R"))
-        self.assertEqual(False, sol.canTransform("LXRX", "LXXR"))
+        self.assertEqual(True, sol.canTransform("LXRX", "LXXR"))
         self.assertEqual(False, sol.canTransform("RLXX", "XXRL"))
         self.assertEqual(True, sol.canTransform("RXXLRXRXL", "XRLXXRRLX"))
         self.assertEqual(True, sol.canTransform("XXXXRXXXLX", "XXXXRLXXXX"))
