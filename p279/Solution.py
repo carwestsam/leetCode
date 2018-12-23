aa77b48c6acbe23d2b4138127341bbf992f7834a
Cedric Sung <carwestsam@gmail.com>
2018-12-23 10:49:35 +0800

    p279: accepted

---
+++ p279/Solution.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def __init__(self):
+        self.dict = {}
+        self.best = 0
+
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # self.best = 10000000
+
+        dict = set()
+        path = [set() for _ in range(2)]
+        side = 0
+        last = 1
+        i = 1
+        ans = 1
+
+        while i * i <= n:
+            dict.add(i * i)
+            path[side].add(i*i)
+            i += 1
+
+        while n not in path[side]:
+            side = 1 - side
+            last = 1 - last
+            path[side] = set()
+            ans += 1
+            for x in path[last]:
+                for y in dict:
+                    # print('got', x+y)
+                    if x + y < n:
+                        path[side].add(x + y)
+                    if x + y == n:
+                        return ans
+
+            print(ans, len(path[side]))
+
+        return ans
---
+++ p279/test_solution.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from p279.Solution import Solution
+
+
+class TestSolution(TestCase):
+    def test_numSquares(self):
+        sol = Solution()
+        # self.assertEqual(3, sol.numSquares(12))
+        # self.assertEqual(2, sol.numSquares(13))
+        # self.assertEqual(2, sol.numSquares(1025))
+        # self.assertEqual(3, sol.numSquares(22))
+        # self.assertEqual(3, sol.numSquares(48))
+        # self.assertEqual(3, sol.numSquares(88))
+        # #
+        self.assertEqual(3, sol.numSquares(1232142))
+        self.assertEqual(3, sol.numSquares(123212342))
